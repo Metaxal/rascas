5afd3a93ac725e35c58810645d800451293db88e
Metaxal <laurent.orseau@gmail.com>
2018-02-11 18:27:59 +0000

    racket package

---
+++ info.rkt
@@ -0,0 +1,8 @@
+#lang info
+(define collection "rascas")
+(define deps '("base"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define scribblings '(("scribblings/rascas.scrbl" ())))
+(define pkg-desc "Computer Algebra System for Racket. Port of dharmatech/mpl.")
+(define version "0.0")
+(define pkg-authors '(orseau))
---
+++ scribblings/rascas.scrbl
@@ -0,0 +1,14 @@
+#lang scribble/manual
+@require[@for-label[rascas
+                    racket/base]
+         racket/base
+         racket/runtime-path]
+
+@title{rascas}
+@author{orseau}
+
+@defmodule[rascas]
+
+Computer Algebra System for Racket.
+
+Port of @hyperlink["https://github.com/dharmatech/mpl"]{dharmatech/mpl}.
