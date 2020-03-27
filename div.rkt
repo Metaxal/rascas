1886879c206be6e92aef4c082f11a8e33ef77156
Metaxal <laurent.orseau@gmail.com>
2020-03-27 12:58:31 +0000

    variadic /

--- div.rkt
+++ div.rkt
@@ -9,19 +9,22 @@
 
 (define (simplify-quotient u)
   (match u
-    [`(/ ,x ,y)
-     (* x (^ y -1))]))
+    [`(/ ,x . ,ys)
+     (* x (^ (apply * ys) -1))]))
 
 (define /
   (case-lambda
-    [(u v) (simplify-quotient `(/ ,u ,v))]
-    [(u) (simplify-quotient `(/ 1 ,u))]))
+    [(u) (simplify-quotient `(/ 1 ,u))]
+    [(u . vs) (simplify-quotient `(/ ,u . ,vs))]))
 
 (module+ test
   (require rackunit)
+  #;(displayln "tests...")
   (check-equal? (/ 4) 1/4)
   (check-equal? (/ 'a) '(^ a -1))
   (check-equal? (/ 3 4) 3/4)
-  (check-equal? (/ 3 'a) '(* 3 (^ a -1))))
+  (check-equal? (/ 3 'a) '(* 3 (^ a -1)))
+  (check-equal? (/ 3 1 2 4) 3/8)
+  (check-equal? (/ 'a 2 (* 2 'a)) 1/4))
 
   
