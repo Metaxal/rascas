dc0cf9bbfe600d7f560933078395662fff21ef6f
Metaxal <laurent.orseau@gmail.com>
2020-03-27 12:54:47 +0000

    allow single argument to /

--- div.rkt
+++ div.rkt
@@ -12,7 +12,16 @@
     [`(/ ,x ,y)
      (* x (^ y -1))]))
 
-(define (/ u v)
-  (simplify-quotient `(/ ,u ,v)))
+(define /
+  (case-lambda
+    [(u v) (simplify-quotient `(/ ,u ,v))]
+    [(u) (simplify-quotient `(/ 1 ,u))]))
+
+(module+ test
+  (require rackunit)
+  (check-equal? (/ 4) 1/4)
+  (check-equal? (/ 'a) '(^ a -1))
+  (check-equal? (/ 3 4) 3/4)
+  (check-equal? (/ 3 'a) '(* 3 (^ a -1))))
 
   
