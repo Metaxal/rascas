7628a1b86e3a95ae8e923d5e74f06f99aeeb19c2
Metaxal <laurent.orseau@gmail.com>
2020-04-05 16:11:17 +0100

    use match for expand-exp

--- expand-exp.rkt
+++ expand-exp.rkt
@@ -6,24 +6,28 @@
 
 (require "arithmetic.rkt"
          "exp.rkt"
-         "misc.rkt")
+         "misc.rkt"
+         racket/match)
 
-(define (expand-exp-rules A)
-  (cond ( (sum? A)
-          (let ((f (list-ref A 1)))
-            (* (expand-exp-rules f)
-               (expand-exp-rules (- A f)))) )
-        ( (product? A)
-          (let ((f (list-ref A 1)))
-            (if (integer? f)
-                (^ (expand-exp-rules (/ A f)) f)
-                (exp A))) )
-        (else (exp A))))
+(define (expand-exp-rules u)
+  (match u
+    [`(+ ,v)
+     (expand-exp-rules v)]
+    [`(+ ,v . ,w)
+     (* (expand-exp-rules v)
+        (expand-exp-rules (cons '+ w)))]
+    [`(* ,v)
+     (expand-exp-rules v)]
+    [`(* ,v . ,w)
+     #:when (integer? v)
+     (^ (expand-exp-rules (cons '* w)) v)]
+    [else
+     (exp u)]))
 
 (define (expand-exp u)
-  (if (or (number? u) (symbol? u))
-      u
-      (let ((v (map expand-exp u)))
-        (if (exp? v)
-            (expand-exp-rules (list-ref v 1))
-            v))))
+  (if (list? u)
+    (match (map expand-exp u)
+      [`(exp ,v)
+       (expand-exp-rules v)]
+      [v v])
+    u))
