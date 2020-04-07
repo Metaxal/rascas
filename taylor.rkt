397452ad5dbb1040d3d9ad61b0af1dd539d31853
Metaxal <laurent.orseau@gmail.com>
2020-04-07 18:13:21 +0100

    add example

--- taylor.rkt
+++ taylor.rkt
@@ -26,3 +26,18 @@
                   (* (exp x0) (+ x (* -1 x0)))
                   (* 1/2 (exp x0) (^ (+ x (* -1 x0)) 2))
                   (* 1/6 (exp x0) (^ (+ x (* -1 x0)) 3)))))
+
+;; Example
+(module+ drracket
+  (require plot
+           "automatic-simplify.rkt")
+  (plot
+   #:width 800
+   #:height 600
+   (list (function (λ (x) (log (->inexact x))) 0 4
+                   #:label "log")
+         (for/list ([i (in-range 4)])
+           (define f (taylor '(log x) 'x 1 i))
+           (function (λ (x) (->inexact (substitute f 'x x)))
+                     #:color (+ i 2)
+                     #:label (format "Approx. log at x=1, order ~a" i))))))
