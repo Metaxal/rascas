ed219ea6ca0da042950419f741cd46b1dce933c9
Metaxal <laurent.orseau@gmail.com>
2020-04-27 18:38:27 +0100

    add substitute/dict, make simplify optional

--- substitute.rkt
+++ substitute.rkt
@@ -9,7 +9,8 @@
          sequential-substitute
          concurrent-substitute
          recurrent-substitute
-         apply//
+         apply// ; wrong name?!
+         substitute/dict
          tree->procedure)
 
 (require racket/match
@@ -40,7 +41,7 @@
   (let loop ([u u])
     (cond [(equal? u t) r]
           [(list? u)
-           (map loop u)]
+           (map loop u)] ; warning: not aware of let*
           [else u])))
 
 ;; Returns u where t has been substituted with r, and whether any substitution has been done.
@@ -67,8 +68,10 @@
 ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Sequentially substitutes all of L into u.
+;; L : (listof (list/c from to))
 ;; automatic-simplify is triggerred only if any change has been made.
-(define (sequential-substitute u L)
+;; WARNING: This function can be slow if S is a large assoc list.
+(define (sequential-substitute u L #:simplify? [simplify? #t])
   (cond
     [(null? L) u]
     [else
@@ -83,12 +86,15 @@
             (when ch? (set! changed? #t))
             (loop new-u rem)])))
      (if changed?
-       (automatic-simplify new-u)
+       (if simplify?
+         (automatic-simplify new-u)
+         new-u)
        u)]))
 
 ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; automatic-simplify is triggerred only if any change has been made and simplify? is #t.
+;; WARNING: This function can be slow if S is a large assoc list.
 ;; TODO: to speed up, specialized variant that just substitutes
 ;; TODO: also allow for a hash table.
 ;; symbols with eq?, and sort the symbols(not sure it's worth it)
@@ -118,6 +124,7 @@
 ;; Alternatively, we could first apply all substitutions inside S recursively,
 ;; and then just do concurrent-substitute. Not sure which is best.
 ;; Reduction is applied only if a change has been made.
+;; WARNING: This function can be slow if S is a large assoc list.
 (define (recurrent-substitute u S [=? equal?])
   (cond
     [(null? S) u]
@@ -148,18 +155,39 @@
     [(u syms vals)
      (unless (= (length syms) (length vals))
        (raise-arguments-error 'apply// "syms and vals must have the same lengths"))
-     (apply// u (make-hasheq (map cons syms vals)))] ; immutable?
-    ;; Substitutions are provided as a dictionary
-    ;; THIS DIFFERS FROM concurrent-substitute
-    [(u subst-dict)
-     (unless (dict? subst-dict)
-       (raise-argument-error 'apply// "subst-dict must be a dict?"))
-     ;; TODO: Apply automatic-simplify only if a change has been detected?
-     (automatic-simplify
-      (let loop ([u u])
-        (cond [(symbol? u) (dict-ref subst-dict u u)]
-              [(list? u)   (map loop u)]
-              [else        u])))]))
+     (substitute/dict u (make-hasheq (map cons syms vals)))] ; immutable?
+    ))
+
+(define no-key (gensym))
+
+;; Replaces in u every occurrence of an symbol id of subst with the corresponding value.
+;; If simplify is #f, no automatic-simplify is applied. Otherwise, automatic-simplify is applied
+;; only if a change is detected.
+;; If recur? is #t, the substitution is recursively applied to any newly substituted value.
+;; u : tree?
+;; subst : (dictof symbol? tree?)
+(define (substitute/dict u subst
+                         #:simplify? [simplify? #t]
+                         #:recur? [recur? #f])
+  (unless (dict? subst)
+    (raise-argument-error 'substitute/dict "dict?" subst))
+  (define changed? #f)
+  (define new-u
+    (let loop ([u u])
+      (define new-u (dict-ref subst u no-key))
+      (cond [(not (eq? new-u no-key))
+             (unless (eq? new-u u)
+                 (set! changed? #t))
+               (if recur?
+                 (loop new-u)
+                 new-u)]
+            [(list? u) (map loop u)] ; warning: this is not aware of let*
+            [else      u])))
+  (if changed?
+    (if simplify?
+      (automatic-simplify new-u)
+      new-u)
+    u))
 
 (module+ test
   (require "arithmetic.rkt")
