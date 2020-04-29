6c8bfb6bd9517c6159389a923e5a9f510192ba0b
Metaxal <laurent.orseau@gmail.com>
2020-04-29 10:33:20 +0100

    fix and speedup jacobian

--- derivative.rkt
+++ derivative.rkt
@@ -4,6 +4,7 @@
 
 (provide derivative
          derivative/proc
+         numeric-derivative ; export only for test submod?
          jacobian)
 
 (require "misc.rkt"
@@ -107,202 +108,34 @@
     (/ (- (f (+ x ε)) (f (- x ε)))
        (* 2 ε))))
 
-(module+ test
-  (require rackunit
-           "automatic-simplify.rkt"
-           "distribute.rkt"
-           "substitute.rkt"
-           "trig-functions.rkt"
-           "special-functions.rkt")
-
-  ;; Tool to test the symbolic derivative value against the numeric one
-  ;; f must be a procedure of 1 argument
-  (define (check-derivative f [xs (build-list 10 (λ (i) (/ (- (random) .5)
-                                                           (random))))])
-    (define dfnum (numeric-derivative f))
-    (define dfsym (derivative/proc f))
-    (for ([v (in-list xs)])
-      (check-= (->inexact
-                (sqr (- (dfnum v)
-                        (dfsym v))))
-               0.
-               0.001)))
-  
-  (check-equal? (derivative (+ (* 3 (^ 'x 2)) (* 4 'x)) 'x)
-                (+ (* 6 'x) 4))
-  (check-equal? (derivative (/ 3 'x) 'x)
-                (/ -3 (sqr 'x)))
-  (check-equal? (derivative (exp (* 3 'x)) 'x)
-                '(* 3 (exp (* 3 x))))
-
-  (check-equal? (derivative (log (* 3 'x)) 'x)
-                '(^ x -1))
-
-  (check-equal? (derivative (sin (* 3 'x)) 'x)
-                '(* 3 (cos (* 3 x))))
-  (check-equal? (derivative (cos (* 3 'x)) 'x)
-                '(* -3 (sin (* 3 x))))
-  (check-equal? (derivative (tan (* 3 'x)) 'x)
-                '(* 3 (^ (cos (* 3 x)) -2)))
-
-  ;; TODO: Check registering a derivative of 3 args
-  
-
-  ; Check that 'derivative is registered as a function
-  (check-equal? (automatic-simplify '(derivative (sqr x) x))
-                (* 2 'x))
-
-  ;; Register a function /after/ it has been used for derivation.
-  ;; Simplify the expression to obtain the correct derivative.
-  (let ()
-    (define bad-deriv (derivative '(__unknown-deriv (* x 2)) 'x))
-    (check-equal? bad-deriv '(derivative (__unknown-deriv (* x 2)) x))
-    (register-function '__unknown-deriv (λ (x) (sqr x)))
-    (check-equal? (automatic-simplify bad-deriv)
-                  (* 8 'x))) 
-
-  (check-derivative gamma (build-list 10 (λ (i) (* 10 (random)))))
 
 
-  ;;; list
 
-  (check-equal? (derivative (_list 'x 'y 'x 'z) 'x)
-                '(list 1 0 1 0))
-
-  (check-equal? (derivative (_list (* 'x 2) (^ 'x 'a) (log 'x)) 'x)
-                '(list 2 (* a (^ x (+ -1 a))) (^ x -1)))
-
-  (check-equal? (derivative (_list (* 'x 2) (^ 'x 'a) (log 'x)) 'a)
-                '(list 0 (* (log x) (^ x a)) 0))
-
-  ;;; let*
-  (check-equal? (derivative (_let* `([a ,(+ 'x 2)])
-                                   (* (+ 1 'a) (+ 2 'a))) 'x)
-                '(+ 3 (* 2 (+ 2 x))))
-  (check-equal? (derivative (expand-let* (_let* `([a ,(+ 'x 2)])
-                                                (* (+ 1 'a) (+ 2 'a)))) 'x)
-                '(+ 7 (* 2 x)))
-  ; error: Cannot differentiate for a bound id (a)
-  (check-exn exn:fail?
-             (λ () (derivative (_let* `([a ,(+ 'x 2)]
-                                        [b ,(* 'a 2)])
-                                      (* (+ 1 'b) (+ 2 'a)))
-                               'a)))
-  (check-equal? (derivative (_let* `([a (+ x 2)])
-                                   (* (+ 1 'a) (+ 2 'a))) 'y)
-                0)
-
-  (check-equal? (distribute-product
-                 (expand-let*
-                  (derivative (_let* `([b (* (+ 1 a) (+ 2 a))] [c (* (+ b 1) (+ b 2))] [d c])
-                                     (* (+ 'd 1) (+ 'd 2)))
-                              'a)))
-                (distribute-product
-                 (derivative (expand-let*
-                              (_let* `([b (* (+ 1 a) (+ 2 a))] [c (* (+ b 1) (+ b 2))] [d c])
-                                     (* (+ 'd 1) (+ 'd 2))))
-                             'a)))
-
-  (check-not-exn
-   (λ () (derivative
-          '(let* ((_u0 (+ (* _w_0_0_0 a) 1))
-                  (_w_out_1_0 (* 0.5 _u0 (+ 1 (sgn _u0))))
-                  (_u1 4)
-                  (_w_out_1_1 (* 0.5 _u1 (+ 1 (sgn _u1))))
-                  (_w_out_2_0
-                   (+ 1.0 (exp (* -1 (+ (* 3 _w_out_1_0) (* 2 _w_out_1_1))))))
-                  (_w_out_2_1
-                   (+ 1.0 (exp (* -1 (+ (* 4 _w_out_1_0) (* 5 _w_out_1_1))))))
-                  (_u2 (+ (* 3 _w_out_2_0) (* 2 _w_out_2_1))))
-             (+ (* 0.5 _u2 _w_3_0_0 (+ 1 (sgn _u2))) (* 0.5 _u2 _w_3_0_1 (+ 1 (sgn _u2)))))
-          '_w_0_0_0)))
-
-  (check-equal?
-   (expand-let*
-    (contract-let*
-     (derivative
-      '(let* ((a (exp x))
-              (b (+ 1.0 a))
-              (c (+ -1.0 a))
-              (d (^ b -2)))
-         (* a (+ (^ b -1) (* -1 d c) (* -1 d a) (* (+ (* -1 d) (* 2 (^ b -3) c)) a))))
-      'x)))
-   (expand-let*
-    '(let* ((_g4 (exp x))
-            (b (+ 1.0 _g4))
-            (c (+ -1.0 _g4))
-            (_g0 (^ b -3))
-            (_g3 (^ b -2))
-            (_g1 (* 2 _g0 c))
-            (_g2 (* -1 _g3)))
-       (*
-        _g4
-        (+
-         (* (+ _g1 _g2) _g4)
-         (* -1 _g3 _g4)
-         (* (+ _g1 (* -2 _g3)) _g4)
-         (* _g4 (+ _g2 (* 2 _g0 _g4)))
-         (^ b -1)
-         (* -1 _g3 c)
-         (* -2 _g0 _g4 (+ (* -2 _g4) (* -1 c)))
-         (* _g4 (+ (* -1 _g3) (* -6 _g4 (^ b -4) c))))))))
-
-  (check-derivative
-   (λ (x)
-     `(let* ((a (exp ,x))
-             (b (+ 1.0 a))
-             (c (+ -1.0 a))
-             (d (^ b -3))
-             (e (^ b -2))
-             (f (* 2 d c))
-             (g (* -1 e)))
-        (*
-         a
-         (+
-          (^ b -1)
-          (* -1 e c)
-          (* -1 e a)
-          (* (+ (* -2 e) f) a)
-          (* (+ (* -1 e) f) a)
-          (* -2 d a (+ (* -1 c) (* -2 a)))
-          (* a (+ g (* 2 d a)))
-          (* a (+ g (* -6 (^ b -4) c a))))))))
-
-
-  (check-equal? ((derivative/proc sqr) 'a)
-                (* 'a 2))
-  (check-equal? ((derivative/proc sqr) 2)
-                4)
-  (check-equal? ((derivative/proc log) 1/2)
-                2)
-  (check-equal? ((derivative/proc log #:inexact? #t) (sqrt 2))
-                (->inexact (/ (sqrt 2))))
-  )
-
-;; --------------------------------------------------------------------------------
-
-;; Reverse mode.
+;; Returns the jacobian of tree on xs, in the form of a _list surrounded
+;; by a _let*, using 'reverse' mode.
+;; Takes advantage of the large amount of shared code between the different derivatives,
+;; which usually leads to a much smaller jacobian tree than length(xs) × size(tree).
+;;
 ;; Go through the bindings in *reverse* order,
 ;; collect the derivative along each branch,
 ;; and when hitting an input symbol x, collect in a hash.
 ;; Finally, build the let* with a list 
 (define (jacobian tree xs)
   (define rev-bindings (make-hash)) ; expr -> id
-  (define bindings (make-hasheq)) ; id -> expr
-  ;; First, create a computation graph
-  ;; WARNING: Does not work with let*
-  (define last-id ; might be a number!
-    (let loop ([tree tree])
+  (define hbindings (make-hasheq)) ; id -> expr
+  ;; Step 1, create a computation graph (bindings are nodes)
+  (define last-id ; might be a number?
+    (let loop ([tree (rebind-all-let* tree)]) ; Should we rebind by default?
       ; Start with the bottom.
       (define res
         (match tree
           [`(let* ,(list subbindings ...) ,body)
            (for ([b (in-list subbindings)])
              (define-values (id expr) (apply values b))
-             (when (hash-has-key? bindings id)
+             (when (hash-has-key? hbindings id)
                (error "ID already exists in hash:" id expr))
              (define new-id (loop expr))
-             (hash-set! bindings id new-id)
+             (hash-set! hbindings id new-id)
              (hash-set! rev-bindings new-id id)) ; not useful?
            ;; TODO: What if the binding already exists?
            (loop body)]
@@ -315,83 +148,56 @@
         [(hash-ref rev-bindings res #f)] ; reuse binding if possible
         [else
          (define id (default-make-id))
-         (hash-set! bindings id res)
+         (hash-set! hbindings id res)
          (hash-set! rev-bindings res id)
          id])))
-  (define jach (make-hasheq))
+  ;; Step 2: backpropagation
+  (define bindings (sort-bindings (hash-map hbindings list)))
+  (define ids (map first bindings))
+  ;; The derivative for each x is updated in the jach hash.
   ;; TODO: Use struct nodes instead of hashes?
+  (define jach (make-hasheq))
   (for ([x (in-list xs)])
     (hash-set! jach x 0))
-  (let loop ([id last-id] [diff 1])
-    (cond [(hash-has-key? jach id)
-           (hash-update! jach id (λ (old) (+ diff old)))]
-          [(hash-ref bindings id #f)
-           =>
-           (λ (expr)
-             ; expr cannot be a let* as they all have been deconstructed.
-             (cond
-               [(number? expr) (void)]
-               [(symbol? expr) (loop expr diff)]
-               [(list? expr)
-                ; for each subid, calculate derivative
-                (for ([subid (in-list (rest expr))])
-                  (define diffid (default-make-id))
-                  (hash-set! bindings diffid (* diff (derivative expr subid)))
-                  (loop subid diffid))]))]
-          [else (void)])) ; number or free id 
-
-  (define body (apply _list (for/list ([x (in-list xs)])
-                              (hash-ref jach x))))
-  #;(debug bindings body)
+  (for ([id (in-list ids)])
+    (hash-set! jach id 0))
+  (hash-set! jach last-id 1) ; WARNING: WHAT IF NUMBER?
+  (define new-bindings
+    (for/fold ([new-bindings '()]) ; are returned in the correct order
+              ([bind (in-list (reverse bindings))])
+      (define-values (id expr) (apply values bind))
+      (define diffid (default-make-id))
+      (cond [(and (symbol? expr)
+                  (hash-has-key? jach expr))
+             ; Chain rule: d.f/d.expr = d.f/d.id × d.id/d.expr + A = d.f/d.id + A
+             ; where A is d.f/d.other-ids.
+             ; diffid will be bound to its derivative d.f/d.id in the resulting let*.
+             (hash-update! jach expr (λ (A) (+ A diffid)))]
+            [(list? expr)
+             ; For each subid (subexpr), calculate derivative.
+             (for ([subid (in-list (rest expr))])
+               (when (and (symbol? subid)
+                          (hash-has-key? jach subid))
+                 ; Chain rule: d.f/d.subid = d.f/d.id × d.id/d.subid + A  (where A is d.f/d.other-ids)
+                 ; diffid will be bound to its derivative d.f/d.id in the resulting let*.
+                 (hash-update! jach subid (λ (A) (+ A (* diffid (derivative expr subid)))))))])
+      ; Once we reach id, all its parents have been processed already,
+      ; so its derivative is definitive and we can propagate to its children.
+      (list* bind (list diffid (hash-ref jach id)) new-bindings)))
+  (define new-body
+    (apply _list
+           (for/list ([x (in-list xs)])
+             (hash-ref jach x))))
+  #;(debug bindings new-bindings new-body)
   ;; Now we can reduce the let*
   ;; The simplify-top is in the likely case where some ids are used 0 times,
   ;; but increase the count of ids in their bound expr. 
-  (simplify-top (_let* (hash-map bindings list) body)))
+  (simplify-top (_let* new-bindings new-body)))
 
-(module+ test
-  (check-equal?
-   (jacobian (+ (* 'c 'a) (* 'd (^ 'b 2)))
-             '(a b))
-   '(list c
-          (* 2 b d)))
 
-  (check-equal?
-   (rebind-all-let*
-    (jacobian (exp (+ (* 'c 'a) (* 'd (^ 'b 2))))
-              '(a b))
-    '_s)
-   '(let* ((_s0 (exp (+ (* a c) (* (^ b 2) d)))))
-      (list (* _s0 c)
-            (* 2 _s0 b d))))
-  
-  (check-equal?
-   (rebind-all-let*
-    (jacobian (sin (exp (+ (* 'c 'a) (* 'd (^ 'b 2)))))
-              '(a b))
-    '_s)
-   '(let* ((_s0 (+ (* a c) (* (^ b 2) d)))
-           (_s1 (* (cos (exp _s0)) (exp _s0))))
-      (list (* _s1 c) (* 2 _s1 b d))))
-
-  ;; let*
-  (check-equal?
-   (rebind-all-let*
-    (jacobian
-     `(let* ([a (+ 3 x)]
-             [b (+ 2 x a)])
-        (+ (* a b)
-           (* (log a) (log b))))
-     '(a b))
-    '_s)
-   '(let* ((_s0 (+ 3 x))
-           (_s1 (+ 2 _s0 x)))
-      (list (+ _s1 (* (^ _s0 -1) (log _s1)))
-            (+ _s0 (* (^ _s1 -1) (log _s0)))))))
 
 ;; Calculates and returns the derivative of each xs and applies the substitutions subst
 ;; at the same time.
 ;; If the substitution is complete and inexact? is not #f, numeric values are returned.
 ;; The returned tree should produce a subst list
 #;(define (diff+subst f xs subst #:inexact? [inexact? #f]))
-
-
