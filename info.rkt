889f0c54b724399ab250e728faf48872d021ddcc
Metaxal <laurent.orseau@gmail.com>
2020-04-30 13:12:10 +0100

    fix deps

--- info.rkt
+++ info.rkt
@@ -4,8 +4,14 @@
                "math-lib"
                "parser-tools-lib"
                "rackunit-lib"
-               "srfi-lite-lib"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+               "srfi-lite-lib"
+               "plot-gui-lib"
+               "plot-lib"
+               "sandbox-lib"))
+(define build-deps '("scribble-lib"
+                     "racket-doc"
+                     "rackunit-lib"
+                     "data-lib"))
 (define scribblings '(("scribblings/rascas.scrbl" ())))
 (define pkg-desc "Computer Algebra System for Racket. Port of dharmatech/mpl.")
 (define version "0.0")
