b4fadeef9761f9f2e5495a1e2cd0e5b7a53e275f
Metaxal <laurent.orseau@gmail.com>
2021-03-24 14:44:20 +0000

    use a let/ec bypass for +nan.0

--- arithmetic.rkt
+++ arithmetic.rkt
@@ -171,7 +171,7 @@
   (check-equal? (^ 0 +nan.0) +nan.0)
   (check-equal? (^ 0 -0.2) +nan.0)
   (check-equal? (^ 0 -2) +nan.0)
-  (check-equal? (^ 'a 0) 1) ; WARNING: only for a≠0
+  (check-equal? (^ 'a 0) 1) ; WARNING: only for a≠0, but numeric before symbolic
   (check-equal? (^ 'a 2) '(^ a 2))
   (check-equal? (^ 'a -4) '(^ a -4))
   (check-equal? (^ (^ 'a 2) 3) '(^ a 6))
@@ -236,67 +236,62 @@
 
 ;; Assumes that subexpressions (powers in particular) have already been reduced.
 (define (* . elts)
-  ; Hash containing the sum of the exponents as a list (may be symbolic).
-  ; The sum is applied at the end.
-  (define counts (make-hash))
-  ; Product of the (real) numbers, initially maintained as a list so as
-  ; to apply the absorption loop at the end (instead of earlier).
-  (define nums '())
-  ;; Count how many elements of each kind, merging products and looking inside powers.
-  (define got-nan?
+  (let/ec return ; +nan.0 bypass
+    ; Hash containing the sum of the exponents as a list (may be symbolic).
+    ; The sum is applied at the end.
+    (define counts (make-hash))
+    ; Product of the (real) numbers, initially maintained as a list so as
+    ; to apply the absorption loop at the end (instead of earlier).
+    (define nums '())
+    ;; Count how many elements of each kind, merging products and looking inside powers.
     (let loop ([l elts])
+      (unless (null? l)
+        (define x (car l))
+        (cond
+          [(eqv? x +nan.0) (return +nan.0)] ; fully absorbing
+          [(number? x)
+           (set! nums (cons x nums))
+           (loop (cdr l))]
+          [(product? x)
+           ; Merge products.
+           (loop (cdr x))
+           (loop (cdr l))]
+          [(power? x)
+           (define ba (base x))
+           (define ex (exponent x))
+           (hash-update! counts ba (λ (exps) (cons ex exps)) '())
+           (loop (cdr l))]
+          [else
+           (hash-update! counts x (λ (exps) (cons 1 exps)) '())
+           (loop (cdr l))])))
+    ; End of loop, return a sorted and compact list
+    (define lres
+      (for/fold ([lres '()]
+                 #:result (sort lres order-relation))
+                ([(x exps) (in-hash counts)])
+        (define pow (^ x (apply + exps)))
+        (cond
+          [(eqv? +nan.0 pow) (return +nan.0)]
+          [(number? pow)
+           (set! nums (cons pow nums))
+           lres]
+          [else (cons pow lres)])))
+    ; Return value.
+    (let ([tot-nums (apply rkt:* nums)])
       (cond
-        [(null? l) #f]
+        [(eqv? +nan.0 tot-nums) +nan.0]
+        [(null? lres)
+         ; Single element (number), remove '*.
+         tot-nums]
+        [(eqv? 0 tot-nums) 0] ; Notice: `eqv?` and not `=`
+        [(and (null? (cdr lres))
+              (= 1 tot-nums))
+         ; Single element (not number), remove '*.
+         (car lres)]
+        [(= 1 tot-nums)
+         (cons '* lres)]
         [else
-         (define x (car l))
-         (cond
-           [(number? x)
-            (cond
-              [(and (real? x) (nan? x)) #t] ; fully absorbing
-              [else
-               (set! nums (cons x nums))
-               (loop (cdr l))])]
-           [(product? x)
-            ; Merge products.
-            (loop (cdr x))
-            (loop (cdr l))]
-           [(power? x)
-            (define ba (base x))
-            (define ex (exponent x))
-            (hash-update! counts ba (λ (exps) (cons ex exps)) '())
-            (loop (cdr l))]
-           [else
-            (hash-update! counts x (λ (exps) (cons 1 exps)) '())
-            (loop (cdr l))])])))
-  (cond
-    [got-nan? +nan.0]
-    [else
-     ; End of loop, return a sorted and compact list
-     (define lres
-       (for/fold ([lres '()]
-                  #:result (sort lres order-relation))
-                 ([(x exps) (in-hash counts)])
-         (define pow (^ x (apply + exps)))
-         (cond
-           [(number? pow)
-            (set! nums (cons pow nums))
-            lres]
-           [else (cons pow lres)])))
-     ; Return value.
-     (let ([tot-nums (apply rkt:* nums)])
-       (cond
-         [(null? lres)
-          ; Single element (number), remove '*.
-          tot-nums]
-         [(= 0 tot-nums) 0] ; 0 * symbolic = 0
-         [(and (null? (cdr lres))
-               (= 1 tot-nums))
-          ; Single element (not number), remove '*.
-          (car lres)]
-         [(= 1 tot-nums)
-          (cons '* lres)]
-         [else
-          `(* ,tot-nums . ,lres)]))]))
+         `(* ,tot-nums . ,lres)]))))
 (register-function '* *)
 
 (module+ test
@@ -335,6 +330,9 @@
   (check-equal? (* 0 +nan.0) +nan.0)
 
   (check-equal? (* 0 'x) 0) ; I'm still unsure about this one
+  (check-equal? (* 0.0 'x) '(* 0.0 x))
+  (check-equal? (* -0.0 'x) '(* -0.0 x))
+  (check-equal? (* -0.0 0.0) -0.0)
   )
 
 ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
